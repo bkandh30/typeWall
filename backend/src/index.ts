d1df91bfbea093ff681cadd220a382cc9966c54c
bkandh30 <kandhari.bhavya20@gmail.com>
2025-08-08 21:54:49 -0700

    initializing api handlers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,26 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+  return c.text('Signup Route')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Signin Route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param('id')
+  console.log(id);
+  return c.text('Get Blog Route')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Signin Post Route')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Signin Put Route')
 })
 
 export default app
