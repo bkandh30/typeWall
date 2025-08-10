52c4b3568fc9d780fb75368b5cab3feba06a1464
bkandh30 <kandhari.bhavya20@gmail.com>
2025-08-09 23:14:17 -0700

    added route to update blog post

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
-import { sign, verify } from 'hono/jwt'
+import { verify } from 'hono/jwt'
 
 export const blogRouter = new Hono<{
     Bindings: {
         DATABASE_URL: string,
         JWT_SECRET: string,
+    },
+    Variables: {
+      userId: string,
     }
 }>();
 
@@ -52,6 +55,23 @@ blogRouter.post('/', async (c) => {
   });
 })
 
-blogRouter.put('/', (c) => {
-  return c.text('Update Blog Route')
+blogRouter.put('/', async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const blog = await prisma.blog.update({
+    where: {
+      id: body.id
+    },
+    data: {
+      title: body.title,
+      content: body.content
+    }
+  })
+
+  return c.json({
+    id: blog.id
+  })
 })
