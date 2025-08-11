c8f126a1fe56320027c71afb2135c669c9b276fb
bkandh30 <kandhari.bhavya20@gmail.com>
2025-08-10 23:16:23 -0700

    added quote component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+export const Quote = () => {
+    return (
+        <div className="bg-slate-200 h-screen flex justify-center flex-col">
+            <div className="flex justify-center">
+                <div className="max-w-lg">
+                    <div className="text-3xl font-bold">
+                        "I've discovered voices here that changed how I think about technology, culture, and creativity. It's not just a blog - it's a university of perspectives."
+                    </div>
+                    <div className="max-w-md text-xl font-semibold text-left mt-4">
+                        David Chen
+                    </div>
+                    <div className="max-w-md text-sm font-light text-slate-600">
+                        Product Designer, Spotify
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
