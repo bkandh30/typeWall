b68d732c027d696fab693f6630eb0f3f995f0aa5
bkandh30 <kandhari.bhavya20@gmail.com>
2025-08-11 00:02:14 -0700

    added signin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import './App.css'
+import { Signin } from './pages/Signin'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
         </Routes>
       </BrowserRouter>
     </>
